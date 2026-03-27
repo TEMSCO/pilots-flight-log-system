072ea74b65b8a8e45b886725268a337c43b3d950
TEMSCO <Joe_hicks@temscoair.com>
2026-03-27 10:42:31 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const supabase = window.supabase.createClient(
+  "https://xjisftmfjsjomgsjzkod.supabase.co",
+  "sb_publishable_5XyiR6l-76O6RFbR1ba2oQ_4p9Gm5oS"
+);
+
+async function login() {
+  console.log("Login clicked");
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+  } else {
+    window.location.href = "/pilot.html";
+  }
+}
